13931b67660efcf8f0bdae339d9dc01b2c19f594
Harsh Shandilya <msfjarvis@gmail.com>
2019-11-07 20:33:47 +0530

    sample: Update provider and key preference handling
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- sample/src/main/kotlin/openpgpktx/sample/SettingsActivity.kt
+++ sample/src/main/kotlin/openpgpktx/sample/SettingsActivity.kt
@@ -6,14 +6,36 @@ package openpgpktx.sample
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import me.msfjarvis.openpgpktx.preference.OpenPgpAppPreference
+import me.msfjarvis.openpgpktx.preference.OpenPgpKeyPreference
+import me.msfjarvis.openpgpktx.util.OpenPgpServiceConnection
+import org.openintents.openpgp.IOpenPgpService2
 
 class SettingsActivity : AppCompatActivity() {
+    private var serviceConnection: OpenPgpServiceConnection? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+        serviceConnection = OpenPgpServiceConnection(
+            applicationContext,
+            PreferenceManager.getDefaultSharedPreferences(applicationContext).getString("provider_app", ""),
+            object : OpenPgpServiceConnection.OnBound {
+                override fun onBound(service: IOpenPgpService2?) {
+                    Toast.makeText(applicationContext, "onBound", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(e: Exception?) {
+                    Toast.makeText(applicationContext, "onError", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+        serviceConnection?.bindToService()
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings, SettingsFragment())
@@ -31,9 +53,22 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceConnection?.unbindFromService()
+        serviceConnection = null
+    }
+
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            val providerPref = findPreference<OpenPgpAppPreference>("provider_app")
+            val keyPref = findPreference<OpenPgpKeyPreference>("pgp_key")
+            keyPref?.setOpenPgpProvider(preferenceManager.sharedPreferences.getString("provider_app", ""))
+            providerPref?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
+                keyPref?.setOpenPgpProvider(newValue as String)
+                true
+            }
         }
     }
 }
